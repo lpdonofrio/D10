b0c28fae5ce8c53a1dc832ec452b390756806776
lpdonofrio <lpdonofrio@gmail.com>
2016-08-16 16:09:36 -0700

    done

--- numbered_lines.py
+++ numbered_lines.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-
+def numbered_lines():
+    with open("small.txt", "r") as fin:
+        with open("small2.txt", "w") as fout:
+            for idx, item in enumerate(fin):
+                fout.write(str(idx + 1) + " " + item)
 
 def main():
     numbered_lines()
